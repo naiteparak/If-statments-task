fb0585e6e8727fd471c96af37f1988a5edc9026e
Ash Karapetyan <ashot.karapetyan.y@gmail.com>
2022-12-08 20:54:42 +0300

    Task

---
+++ average/average.js
@@ -0,0 +1,9 @@
+const xUser = Math.floor((Math.random() * 100) + 1);
+const yUser = Math.floor((Math.random() * 100) + 1);
+const zUser = Math.floor((Math.random() * 100) + 1); //🤨
+
+console.log(xUser, yUser, zUser)
+
+//if(averageAge){
+// Write your code here
+// }
