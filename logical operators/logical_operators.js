bddd11125f3c5dc5705919c261f1e9311cd898c4
soicy <soicyinyourmind@gmail.com>
2022-12-09 12:21:45 +0300

    Implemented prompt style calculation of average age and then checking it

--- logical operators/logical_operators.js
+++ logical operators/logical_operators.js
@@ -1,7 +1,27 @@
-const xUser = prompt("How old are you?");
-const yUser = prompt("How old are you?");
-const zUser = prompt("How old are you?");
+let xUser = prompt("How old are you?");
+let yUser = prompt("How old are you?");
+let zUser = prompt("How old are you?");
 
-//if(averageAge){
-// Write your code here
-// }
+xUser = Number(xUser);
+yUser = Number(yUser);
+zUser = Number(zUser);
+
+if (!(isNaN(xUser)) && !(isNaN(yUser)) && !(isNaN(zUser))) {
+
+    averageAge = (xUser + yUser + zUser) / 3;
+
+
+    if (averageAge >= 60) {
+        console.log('Average age of users more than 60');
+    } 
+
+    else if (averageAge < 60) {
+        console.log('Average age of users less than 60');
+    }
+
+}   
+
+
+else {
+    console.log('I think you entered something wrong');
+}
